f8cc89b546564bce93591571a764767b508463af
Dilnaz Zhumabaikyzy <dilnazzh200303@gmail.com>
2024-04-10 04:28:30 +0600

    test railway

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
 import os
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+class WelcomeRailway(Resource):
+    def get(self):
 
+        response = "Hello from ml-server!"
+
+        return jsonify({"message": response}), 200
+    
+api.add_resource(WelcomeRailway, "/")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
